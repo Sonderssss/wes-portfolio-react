6ff9fa01d45fd106ba34217f1d85dc092544a9a8
sonderssss <emmanuelwaweru222199@daystar.ac.ke>
2026-03-10 13:19:08 +0300

    refactor: improve formatting and readability of navigation links in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import AppRouter from './router'
-import './App.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import AppRouter from "./router";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <>
       <nav>
         <div className="container">
-          <NavLink to="/" className={({ isActive }) => isActive ? 'router-link-exact-active' : ''}>Home</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "router-link-exact-active" : ""
+            }
+          >
+            HOME
+          </NavLink>
           <span>WES</span>
           <div className="other-links">
-            <NavLink to="/projects" className={({ isActive }) => isActive ? 'router-link-exact-active' : ''}>Projects</NavLink>
-            <NavLink to="/about" className={({ isActive }) => isActive ? 'router-link-exact-active' : ''}>About</NavLink>
-            <NavLink to="/contact" className={({ isActive }) => isActive ? 'router-link-exact-active' : ''}>Contact</NavLink>
+            <NavLink
+              to="/projects"
+              className={({ isActive }) =>
+                isActive ? "router-link-exact-active" : ""
+              }
+            >
+              PROJECTS
+            </NavLink>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? "router-link-exact-active" : ""
+              }
+            >
+              ABOUT
+            </NavLink>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? "router-link-exact-active" : ""
+              }
+            >
+              CONTACT
+            </NavLink>
           </div>
         </div>
       </nav>
       <AppRouter />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
