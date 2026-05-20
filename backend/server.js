4e62129f01577f92f6eaeafd4b0aba4dd93dbacb
sonderssss <emmanuelwaweru222199@daystar.ac.ke>
2026-05-20 17:40:56 +0300

    feat:  express.json middleware to sit on top of routes

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ import routes from "../backend/routes/routes.js";
 const app = express();
 
 app.use(cors());
-app.use(routes);
 app.use(express.json());
+app.use(routes);
 
 app.get("/", (req, res) => {
   res.json({
