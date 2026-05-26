94c57474adb45ee32c95aba2f7edd25ebcd77034
sonderssss <emmanuelwaweru222199@daystar.ac.ke>
2026-05-26 19:44:56 +0300

    fix: comment out headers in axios configuration

--- src/services/axios.js
+++ src/services/axios.js
@@ -4,9 +4,9 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // if using cookies
   timeout: 10000, // optional timeout
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
 });
 
 export default api;
